f3c107b8835188f984a3a34a706efc7b7f98ce5b
Massimiliano Arione <garakkio@gmail.com>
2025-09-04 09:08:49 +0200

    ⬆️  allow Symfony 8

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,7 +1,7 @@
 <?php
 // see https://github.com/FriendsOfPHP/PHP-CS-Fixer
 
-$finder = PhpCsFixer\Finder::create()
+$finder = (new PhpCsFixer\Finder())
     ->in([__DIR__.'/src', __DIR__.'/tests'])
 ;
 
@@ -12,10 +12,8 @@ return (new PhpCsFixer\Config())
         '@Symfony:risky' => true,
         '@PHP81Migration' => true,
         '@PHPUnit100Migration:risky' => true,
-        'ordered_imports' => true,
         'declare_strict_types' => false,
         'native_function_invocation' => ['include' => ['@all']],
-        'php_unit_mock_short_will_return' => true,
     ])
     ->setFinder($finder)
 ;
